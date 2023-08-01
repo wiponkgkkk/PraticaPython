5d08a0786958076562913bcc77890c8af54887ed
William José <williamdelas222@gmail.com>
2023-08-01 11:48:31 -0300

    Segundo commit exercicio - aula

--- ex-fixacao02.py
+++ ex-fixacao02.py
@@ -1,10 +1,5 @@
-def calcular_conta_com_garcom(valor_conta):
-    gorjeta = valor_conta * 0.10
-    total_conta_com_garcom = valor_conta + gorjeta
-    return total_conta_com_garcom
+num = float(input("Qual valor da conta"))
 
+Res = num + (num*0.10)
 
-valor_conta = float(input("Digite o valor da conta: "))
-valor_total_com_garcom = calcular_conta_com_garcom(valor_conta)
-
-print(f"O valor total da conta com 10% do garçom é: R$ {valor_total_com_garcom:.2f}")
+print("Resultado", Res)
